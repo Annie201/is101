5043495265245799493ea0d8b8dee26c56d48420
Annie201 <Annie201@users.noreply.github.com>
2015-11-27 18:11:45 +0900

    Update wordcloud.R

--- wordcloud.R
+++ wordcloud.R
@@ -11,7 +11,7 @@ dh.nouns <- sapply(dh.lines, extractNoun, USE.NAMES=F)
 dh.data <- unlist(dh.nouns)
 dh.data2 <- Filter(function(x){nchar(x)>MINIMUM LENGTH OF WORD HERE}, dh.data)
 dh.data2 <- gsub("STOPWORD", "", dh.data2)
-# ""삭제를 위해 아래 줄이 수정되었습니다. 추가 명렁어는 없습니다.
+# ""삭제를 위해 아래 줄이 수정되었습니다. 추가 명령어는 없습니다.
 dh.wordcount2 <- table(unlist(dh.data2), exclude="")
 df.dh.wordcount2 <- as.data.frame(dh.wordcount2)
 
